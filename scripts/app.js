55588d98b75fd9baa9a1ac77460af93da1ec1639
Tian-Who <cbhomeacademy@gmail.com>
2024-08-21 14:12:19 -0600

    Added a door and a guy that just wants to be left alone

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,13 @@ const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+
+var cat = 1;
+
+var Button = false;
+function CAT() {
+	console.log("worow");
+	cat +++ 1;
+	console.log(cat);
+}
+addEventListener("click", CAT);
---
+++ scripts/hey-there.js
@@ -0,0 +1,7 @@
+var WOW = 0;
+function stopTouchingME() {
+	console.log("HEY STOP TOUCHING ME!!! >:[");
+	WOW++ + 1;
+	console.log(WOW);
+}
+addEventListener("click", stopTouchingME);
