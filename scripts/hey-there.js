ac776962eac431dea3c0eecb44b322f3e15a1540
Tian-Who <cbhomeacademy@gmail.com>
2024-09-11 12:38:57 -0600

    Added playing messages at random

--- scripts/hey-there.js
+++ scripts/hey-there.js
@@ -9,26 +9,40 @@ let gotPast10 = false;
 
 let gotPast50 = false;
 
+let messages = [
+	0,
+
+	1,
+
+	2,
+];
 speak.innerText = "Why are you in here??? WHO ARE YOU!?!??!??!?";
 
 function stopTouchingME() {
-	if (WOW == 10) {
-		gotPast10 = true;
-	} else if (WOW == 50) {
-		(gotPast10 = false), (gotPast50 = true);
-	} else if (WOW == 100) {
-		(WOW = 0), (gotPast10 = false), (gotPast10 = false);
-	}
-	WOW++ + 1;
-	if (gotPast10 === false && gotPast50 === false) {
+	let randomMessage = Math.floor(Math.random() * messages.length);
+	randomMessage;
+	if (randomMessage === 0) {
 		speak.innerText = "HEY STOP TOUCHING ME!!! >:[";
-	} else if (gotPast10 == true) {
+	} else if (randomMessage === 1) {
 		speak.innerText = "YOU BETTER STOP!! >:[ >:[";
-	} else if (gotPast50 == true) {
+	} else if (randomMessage === 2) {
 		speak.innerText = "STOP OR ELSE >:[ >:[ >:[";
 	}
-
-	console.log(WOW);
+	// if (WOW == 10) {
+	// 	gotPast10 = true;
+	// } else if (WOW == 50) {
+	// 	(gotPast10 = false), (gotPast50 = true);
+	// } else if (WOW == 100) {
+	// 	(WOW = 0), (gotPast10 = false), (gotPast10 = false);
+	// }
+	// WOW++ + 1;
+	// if (gotPast10 === false && gotPast50 === false) {
+	// 	speak.innerText = "HEY STOP TOUCHING ME!!! >:[";
+	// } else if (gotPast10 == true) {
+	// 	speak.innerText = "YOU BETTER STOP!! >:[ >:[";
+	// } else if (gotPast50 == true) {
+	// 	speak.innerText = "STOP OR ELSE >:[ >:[ >:[";
+	console.log(randomMessage);
 }
 
 addEventListener("click", stopTouchingME);
